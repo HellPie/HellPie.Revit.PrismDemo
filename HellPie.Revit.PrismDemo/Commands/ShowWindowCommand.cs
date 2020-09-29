ac46094a8f6cb5c162d1e9b01053f7eb9cb3719a
Diego Rossi <git@hellpie.dev>
2020-09-29 17:10:33 +0200

    Fire off the view with an up to date region manager from an external Revit command

--- HellPie.Revit.PrismDemo/Commands/ShowWindowCommand.cs
+++ HellPie.Revit.PrismDemo/Commands/ShowWindowCommand.cs
@@ -1,10 +1,19 @@
 ﻿using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
+using HellPie.Revit.PrismDemo.Prism;
+using HellPie.Revit.PrismDemo.Views;
+using Prism.Ioc;
+using Prism.Regions;
 
 namespace HellPie.Revit.PrismDemo.Commands {
     public class ShowWindowCommand : IExternalCommand, IExternalCommandAvailability {
         /// <inheritdoc />
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements) {
+            SinglePrismWindow window = PrismUtils.GetContainer().Resolve<SinglePrismWindow>();
+            RegionManager.SetRegionManager(window, PrismUtils.GetContainer().Resolve<IRegionManager>());
+            RegionManager.UpdateRegions();
+            window.ShowDialog();
+
             return Result.Succeeded;
         }
 
