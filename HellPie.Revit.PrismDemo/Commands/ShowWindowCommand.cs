fc92623a29df60675ac512dfbf145168bc663637
Diego Rossi <git@hellpie.dev>
2020-10-06 14:32:34 +0200

    Simplify window creation by wrapping IoC setup in a utils call

--- HellPie.Revit.PrismDemo/Commands/ShowWindowCommand.cs
+++ HellPie.Revit.PrismDemo/Commands/ShowWindowCommand.cs
@@ -3,19 +3,13 @@ using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using HellPie.Revit.PrismDemo.Prism;
 using HellPie.Revit.PrismDemo.Views;
-using Prism.Ioc;
-using Prism.Regions;
 
 namespace HellPie.Revit.PrismDemo.Commands {
     [Transaction(TransactionMode.ReadOnly)]
     public class ShowWindowCommand : IExternalCommand, IExternalCommandAvailability {
         /// <inheritdoc />
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements) {
-            SinglePrismWindow window = new SinglePrismWindow();
-            RegionManager.SetRegionManager(window, PrismUtils.GetContainer().Resolve<IRegionManager>());
-            RegionManager.UpdateRegions();
-            window.ShowDialog();
-
+            PrismUtils.CreateWindow<SinglePrismWindow>().ShowDialog();
             return Result.Succeeded;
         }
 
--- HellPie.Revit.PrismDemo/Prism/PrismUtils.cs
+++ HellPie.Revit.PrismDemo/Prism/PrismUtils.cs
@@ -1,9 +1,19 @@
-﻿using Prism.Ioc;
+﻿using System;
+using System.Windows;
+using Prism.Ioc;
+using Prism.Regions;
 
 namespace HellPie.Revit.PrismDemo.Prism {
     public static class PrismUtils {
         public static IContainerProvider GetContainer() {
             return PrismExternalApplication.Instance.Container;
+        }
+
+        public static T CreateWindow<T>() where T : Window {
+            T window = Activator.CreateInstance<T>();
+            RegionManager.SetRegionManager(window, GetContainer().Resolve<IRegionManager>());
+            RegionManager.UpdateRegions();
+            return window;
         }
     }
 }
